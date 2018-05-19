92035804ab61844761013680b9c52d2c60008bef
Michael Fuery <mfuery@gmail.com>
2018-05-19 14:21:05 -0700

    better models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import PledgeGroup, Depot, UserProfile, NeededItem
+
+
+class PledgeGroupAdmin(admin.ModelAdmin):
+    model = PledgeGroup
+
+
+class NeededItemAdmin(admin.ModelAdmin):
+    model = NeededItem
+
+
+class DepotAdmin(admin.ModelAdmin):
+    model = Depot
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    model = UserProfile
