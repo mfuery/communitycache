6e427ee58e82c23b6ff59de4c6945cbd81fb2082
Stephen Han <stephenhan@gmail.com>
2018-05-19 16:28:24 -0700

    Simple API up for depot

--- api/serializers.py
+++ api/serializers.py
@@ -5,17 +5,22 @@ from core.models import Depot, Item, Pledge
 
 class DepotSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=200)
-    items = serializers.RelatedField(source='item')
+    items = serializers.RelatedField(source='item', read_only=True)
 
     class Meta:
         model = Depot
+        fields = '__all__'
 
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = '__all__'
+
 
 
 class PledgeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pledge
+        fields = '__all__'
+
--- api/views.py
+++ api/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-
 # Create your views here.
-from rest_framework import viewsets, serializers
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from api.serializers import DepotSerializer, ItemSerializer, PledgeSerializer
 from core.models import Depot, Item, Pledge
@@ -11,7 +7,7 @@ from core.models import Depot, Item, Pledge
 
 class DepotViewSet(viewsets.ModelViewSet):
     queryset = Depot.objects.all()
-    serializers = DepotSerializer
+    serializer_class = DepotSerializer
 
 
 class ItemViewSet(viewsets.ModelViewSet):
