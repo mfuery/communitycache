76d49b9c2c546f88276448ca11eed175a616cfa7
Michael Fuery <mfuery@gmail.com>
2018-05-19 15:01:04 -0700

    data layer distance methods

--- core/models.py
+++ core/models.py
@@ -1,16 +1,25 @@
+from logging import getLogger
+from math import sin, cos, atan2, sqrt
+
 from django.contrib.auth.models import User
 from django.db import models
 
+logger = getLogger()
 
-# class ItemStatus(models.Model):
-#     """
-#     Probably not needed
-#
-#     Needed
-#     Pledged
-#     Fulfilled
-#     """
-#     status = models.CharField(max_length=255)
+
+def get_distance(lat1, lon1, lat2, lon2):
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat2 = lat1
+    lon2 = lon1
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return R * c
 
 
 class PledgeGroup(models.Model):
@@ -29,7 +38,7 @@ class NeededItem(models.Model):
     expires_on = models.DateTimeField()
     quantity = models.IntegerField(default=1)
     value_per_unit = models.FloatField(default=0.00)
-    pledge_group = models.ForeignKey(PledgeGroup, on_delete=models.CASCADE)
+    pledge_group = models.ForeignKey(PledgeGroup, on_delete=models.DO_NOTHING)
 
 
 class Depot(models.Model):
@@ -47,6 +56,41 @@ class UserProfile(models.Model):
     User's lat/lon defines where the NeededItems are. (The user is carrying them)
     A user might have 12 rolls... that contributes to the PledgeGroup.
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     lat = models.FloatField()
     lon = models.FloatField()
+    home_depot = models.ForeignKey(Depot, on_delete=models.DO_NOTHING)
+
+    def get_closest_depot(self):
+        """
+        Call to google maps api
+        :return:
+        """
+        distances = {}
+        depots = Depot.objects.all()
+
+        for depot in depots:
+            distances[depot.id] = get_distance(self.lat, self.lon, depot.lat, depot.lon)
+
+            logger.debug(
+                f'lat/lon user:{self.lat},{self.lon} | '
+                f'depot{depot.id}:{depot.lat},{depot.lon} | '
+                f'dist={distances[depot.id]}km')
+
+        depot_id = min(distances, key=distances.get)
+        closest_depot = Depot.objects.get(pk=depot_id)
+        return {
+            'name': closest_depot.name,
+            'lat': closest_depot.lat,
+            'lon': closest_depot.lon,
+        }
+
+    def get_closest_needed_items(self, count=10):
+        return [{
+            'name': '',
+            'image_url': '',
+            'quantity': '',
+            'lat': '',
+            'lon': '',
+            'distance': '',
+        }]
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+
+class UserProfileModelTestCase(TestCase):
+
+    def test_get_closest_depot(self):
+        """
+        Todo
+        """
+        test_cases = [
+            {
+                'lat1': 37.383825,
+                'lon1': -122.012746,
+                'lat2': 37.383825,
+                'lon2': -122.012746,
+                'distance': 0,
+            }
+        ]
+
+        user = User()
+        user.save()
