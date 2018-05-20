32431c8423bf46d8d13bb3e4db958cda5c1baf69
Michael Fuery <mfuery@gmail.com>
2018-05-19 20:25:02 -0700

    fix get distance and get fns

--- core/models.py
+++ core/models.py
@@ -11,11 +11,8 @@ def get_distance(lat1, lon1, lat2, lon2):
     # approximate radius of earth in km
     R = 6373.0
 
-    lat2 = lat1
-    lon2 = lon1
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
@@ -45,12 +42,16 @@ class Depot(models.Model):
     Nationwide is on your side.
     """
     name = models.CharField(max_length=255)
-    # location = geomodels.PointField()
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
 
     def get_pledges(self):
-        pledges = Pledge.objects.all()
+        pledges = Pledge.objects.filter(
+            need__depot=self
+        )
+
+        # for pledge in pledges:
+
         return pledges
 
     def get_needed_items(self):
@@ -74,7 +75,6 @@ class UserProfile(models.Model):
     A user might have 12 rolls... that creates a new Pledge.
     """
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    # location = geomodels.PointField()
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
 
@@ -83,23 +83,29 @@ class UserProfile(models.Model):
         :return: Depot model
         """
         # Only 1 depot for the demo.
-        # distances = {}
-        # depots = Depot.objects.all()
-        #
-        # for depot in depots:
-        #     distances[depot.id] = get_distance(self.lat, self.lon, depot.lat, depot.lon)
-        #
-        #     logger.debug(
-        #         f'lat/lon user:{self.lat},{self.lon} | '
-        #         f'depot{depot.id}:{depot.lat},{depot.lon} | '
-        #         f'dist={distances[depot.id]}km')
-        #
-        # depot_id = min(distances, key=distances.get)
-        # closest_depot = Depot.objects.get(pk=depot_id)
+        distances = {}
+        depots = Depot.objects.all()
 
-        closest_depot = Depot.objects.all()[0]
+        for depot in depots:
+            distances[depot.id] = get_distance(self.lat, self.lon, depot.lat, depot.lon)
+
+            logger.debug(
+                f'lat/lon user:{self.lat},{self.lon} | '
+                f'depot{depot.id}:{depot.lat},{depot.lon} | '
+                f'dist={distances[depot.id]}km')
+
+        depot_id = min(distances, key=distances.get)
+        closest_depot = Depot.objects.get(pk=depot_id)
         return closest_depot
 
     def get_closest_needed_items(self, count=10):
         items = Item.objects.all()
-        return items
+        closest_depot = self.get_closest_depot()
+
+        distances = {}
+
+        for item in items:
+            distances[item.id] = get_distance(self.lat, self.lon, closest_depot.lat, closest_depot.lon)
+
+        nearby_items = min(distances, key=distances.get)
+        return nearby_items
