5a608b2803abae924b8572db5f0447ff4fc81726
jkang8 <johnkang.h@gmail.com>
2018-05-20 11:57:41 -0700

    Merge packagejson

--- assets/js/map.jsx
+++ assets/js/map.jsx
@@ -1,62 +1,76 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {GoogleMap, Marker, withGoogleMap, DirectionsRenderer} from 'react-google-maps';
 
 
-export class MapContainer extends Component {
-  constructor(props) {
+const Map = withGoogleMap((props) => {
+  return <GoogleMap
+    defaultZoom={16}
+    defaultCenter={{ lat: props.lat, lng: props.lng }}
+  >
+    {<Marker position={{ lat: props.lat, lng: props.lng }} />}
+    {props.directions && <DirectionsRenderer directions={props.directions} />}
+
+  </GoogleMap>
+});
+
+export default class MapContainer extends Component {
+  constructor() {
     super();
-    this.state = {
-      activeMarker: {},
-      selectedPlace: {},
-      showingInfoWindow: true
-    }
+    this.state = {directions: null}
   }
-  onInfoWindowClose() {
-
-  }
-  onMarkerClick(props, marker, e) {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
+  componentDidMount() {
+    const DirectionsService = new google.maps.DirectionsService();
+    let item = this.props.location.state.item;
+    let currLat = 37.900000;
+    let currLng = -122.300000;
+    let destLat = this.props.location.state.lat;
+    let destLng = this.props.location.state.lng;
+    DirectionsService.route({
+      origin: new google.maps.LatLng(currLat, currLng),
+      destination: new google.maps.LatLng(destLat, destLng),
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (result, status) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        this.setState({
+          directions: result,
+        });
+      } else {
+        console.error(`error fetching directions ${result}`);
+      }
     });
   }
-
-  onMapClicked(props) {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      })
-    }
-  }
   render() {
-    let lat = this.props.location.state.lat;
-    let lng = this.props.location.state.lng;
-    let position = {lat, lng};
-    let item = this.props.location.state.item;
-    return (
-      <Map google={this.props.google} zoom={14} initialCenter={position} onClick={this.onMapClicked.bind(this)}>
-
-        <Marker onClick={this.onMarkerClick.bind(this)}
-                name={item.depot.name}
-        />
-
-        <InfoWindow
-          marker={this.state.activeMarker}
-          onClose={this.onInfoWindowClose.bind(this)}
-          visible={this.state.showingInfoWindow}
-        >
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
-    );
+    return <Map
+      isMarkerShown={true}
+      onMarkerClick={this.handleMarkerClick}
+      containerElement={<div style={{ height: `400px` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+      directions={this.state.directions}
+      lat={parseFloat(this.props.location.state.lat)}
+      lng={parseFloat(this.props.location.state.lng)}
+    />
   }
 }
+// export class MapContainer extends Component {
+//   constructor(props) {
+//     super();
+//   }
+//   render() {
+//     let lat = this.props.location.state.lat;
+//     let lng = this.props.location.state.lng;
+//     let position = {lat, lng};
+//     let item = this.props.location.state.item;
+//     return (<div>
+//       <GoogleMap
+//         defaultZoom={8}
+//         defaultCenter={{ lat: -34.397, lng: 150.644 }}
+//       >
+//       </GoogleMap>
+//     </div>);
+//   }
+// }
 
 
-export default GoogleApiWrapper({
-  apiKey: ("AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo")
-})(MapContainer)
+// export default GoogleApiWrapper({
+//   apiKey: ("AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo")
+// })(MapContainer)
